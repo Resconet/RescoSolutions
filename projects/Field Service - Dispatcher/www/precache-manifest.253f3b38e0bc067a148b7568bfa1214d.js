13c886775d0a8f17f0af68bd207b847f4c848b37
Richard Fric <richard.fric@resco.net>
2022-02-08 09:36:42 +0100

    Add Field Service projects

---
+++ projects/Field Service - Dispatcher/www/precache-manifest.253f3b38e0bc067a148b7568bfa1214d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87947dc23f0e5d1d4747a673ef7999f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f20a9f2519c114973bf2",
+    "url": "./static/css/main.1ee50553.chunk.css"
+  },
+  {
+    "revision": "f30de3c370b2c8c8b02e",
+    "url": "./static/js/2.4223778b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.4223778b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20a9f2519c114973bf2",
+    "url": "./static/js/main.2298541d.chunk.js"
+  },
+  {
+    "revision": "608f67c73dbcc57b2523",
+    "url": "./static/js/runtime-main.dd8f46c1.js"
+  }
+]);
